c93e333d395975748bed8044a741c2de2b7df35a
Keath Milligan <keath@keathmilligan.net>
2017-12-29 14:31:18 -0600

    Initial Commit

---
+++ MacOpenCVTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOpenCVTest
+//
+//  Created by Keath Milligan on 12/29/17.
+//  Copyright © 2017 Keath Milligan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
